b4dc9cf711ae9ccf85a60b0c9e63cd31961f32f0
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2024-11-20 14:44:56 +0000

    CDPS-1054: Remove wildcard import.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/common/config/UserEnhancedOAuth2ClientCredentialGrantRequestConverter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/common/config/UserEnhancedOAuth2ClientCredentialGrantRequestConverter.kt
@@ -5,7 +5,7 @@ import org.springframework.http.RequestEntity
 import org.springframework.security.oauth2.client.endpoint.OAuth2ClientCredentialsGrantRequest
 import org.springframework.security.oauth2.client.endpoint.OAuth2ClientCredentialsGrantRequestEntityConverter
 import org.springframework.util.MultiValueMap
-import java.util.*
+import java.util.Objects
 
 @SuppressWarnings("unchecked")
 class UserEnhancedOAuth2ClientCredentialGrantRequestConverter :
