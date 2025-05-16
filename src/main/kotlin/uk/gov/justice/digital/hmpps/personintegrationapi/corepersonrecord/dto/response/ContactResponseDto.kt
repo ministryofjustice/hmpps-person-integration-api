b3a6b7317eab645237a357976318f0491dcbf1b2
Michael Clancy <92720613+mtac50@users.noreply.github.com>
2025-05-16 10:48:21 +0100

    CPDS-1404: Template API for global numbers and emails  (#47)
    * CDPS-1404: Template API added for global number and emails.
    
    * CDPS-1404: Aligned contact types with NOMIS values.
    
    * CDPS-1404: Linting fixes applied.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/corepersonrecord/dto/response/ContactResponseDto.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+
+@Schema(description = "Contact response object containing phone number or email contact details for a person.")
+data class ContactResponseDto(
+
+  @Schema(
+    description = "The ID of the person the reference is associated with. " +
+      "While NOMIS is the data source the person ID will be the prisoner number.",
+    example = "true",
+  )
+  @field:NotNull
+  val personId: String,
+
+  @Schema(
+    description = "Contact type",
+    example = "HOME",
+    allowableValues = ["HOME", "BUS", "FAX", "ALTB", "ALTH", "MOB", "VISIT", "EMAIL"],
+  )
+  @field:NotNull
+  val contactType: String,
+
+  @Schema(description = "Contact value", example = "01234 567 789")
+  @field:Size(max = 240)
+  @field:NotNull
+  val contactValue: String,
+)
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/corepersonrecord/resource/ContactV1Resource.kt
@@ -0,0 +1,199 @@
+package uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.personintegrationapi.common.annotation.ValidPrisonerNumber
+import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.CorePersonRecordRoleConstants
+import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.request.ContactRequestDto
+import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.response.ContactResponseDto
+import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.service.ContactsService
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+
+@RestController
+@Tag(
+  name = "Contacts V1",
+  description = "Contact details for (Phone/Email) for a HMPPS person.",
+)
+@RequestMapping(value = ["v1/person/{personId}"], produces = [APPLICATION_JSON_VALUE])
+class ContactV1Resource(
+  private val contactsService: ContactsService,
+) {
+
+  @GetMapping(
+    "/contacts",
+    produces = [APPLICATION_JSON_VALUE],
+  )
+  @ResponseStatus(HttpStatus.OK)
+  @Operation(
+    summary = "Get all contacts details associated with the given person.",
+    description = "Returns the list of contact details for the person. " +
+      "Requires role `${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_ROLE}` or `${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}`",
+    parameters = [
+      Parameter(
+        name = "personId",
+        description = "The identifier for the person. While NOMIS is the underlying datasource this is the prisoner number",
+        example = "A1234AA",
+      ),
+    ],
+    responses = [
+      ApiResponse(responseCode = "200", description = "Successful response."),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Missing required role. Requires either ${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_ROLE} " +
+          "or `${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}`",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Person not found",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_ROLE}', '${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}')")
+  fun getContacts(
+    @PathVariable(required = true) @Valid @ValidPrisonerNumber personId: String,
+  ): ResponseEntity<Collection<ContactResponseDto>> = contactsService.getContacts(personId)
+
+  @PostMapping("/contacts")
+  @Operation(
+    summary = "Create new contact details for the given person.",
+    description = "Creates a contact. " +
+      "Requires role `${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}`",
+    parameters = [
+      Parameter(
+        name = "personId",
+        description = "The identifier for the person. While NOMIS is the underlying datasource this is the prisoner number",
+        example = "A1234AA",
+      ),
+    ],
+    responses = [
+      ApiResponse(responseCode = "201", description = "Contact successfully created."),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Missing required role. Requires ${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Person not found",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  @PreAuthorize("hasRole('${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}')")
+  fun createContact(
+    @PathVariable(required = true) @Valid @ValidPrisonerNumber personId: String,
+    @RequestBody(required = true) @Valid contactRequest: ContactRequestDto,
+  ): ResponseEntity<ContactResponseDto> = contactsService.createContact(personId, contactRequest)
+
+  @PutMapping("/contacts/{contactId}")
+  @Operation(
+    summary = "Update a contact for the given person.",
+    description = "Updates a contact. " +
+      "Requires role `${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}`",
+    parameters = [
+      Parameter(
+        name = "personId",
+        description = "The identifier for the person. While NOMIS is the underlying datasource this is the prisoner number",
+        example = "A1234AA",
+      ),
+    ],
+    responses = [
+      ApiResponse(responseCode = "200", description = "Contact successfully updated."),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Missing required role. Requires ${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Person or reference not found",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  @PreAuthorize("hasRole('${CorePersonRecordRoleConstants.CORE_PERSON_RECORD_READ_WRITE_ROLE}')")
+  fun updateContact(
+    @PathVariable(required = true) @Valid @ValidPrisonerNumber personId: String,
+    @PathVariable(required = true) @Valid contactId: Long,
+    @RequestBody(required = true) @Valid contactRequest: ContactRequestDto,
+  ): ResponseEntity<ContactResponseDto> = contactsService.updateContact(personId, contactId, contactRequest)
+}
