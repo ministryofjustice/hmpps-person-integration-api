2ae048da0ace36e0bba6e84dc98f7022b1265eeb
Jon Brighton <brightonsbox@hotmail.com>
2025-07-28 11:45:41 +0100

    CDPS-1562: Temporary change to support change in response model from Prison API (#76)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/common/dto/ReferenceDataValue.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/common/dto/ReferenceDataValue.kt
@@ -7,11 +7,11 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(description = "Reference Data Value - a reference data code selected as the value for a field")
 @JsonInclude(NON_NULL)
 data class ReferenceDataValue(
-  @Schema(description = "Id", example = "id")
-  val id: String,
+//  @Schema(description = "Id", example = "id")
+//  val id: String?,
 
   @Schema(description = "Code", example = "code")
-  val code: String,
+  val code: String?,
 
   @Schema(description = "Description of the reference data code", example = "Code description")
   val description: String,
