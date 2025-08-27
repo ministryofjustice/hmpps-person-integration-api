d9cc72e18e58edd7cb110c107fcc2e85b382dbde
Ewan Donovan <ewan.donovan@digital.justice.gov.uk>
2025-08-27 16:00:38 +0100

    Removed unused function

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/common/client/request/SourceSystem.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/common/client/request/SourceSystem.kt
@@ -3,5 +3,3 @@ package uk.gov.justice.digital.hmpps.personintegrationapi.common.client.request
 enum class SourceSystem {
   NOMIS,
 }
-
-fun String.toSourceSystem() = SourceSystem.valueOf(this.trim().uppercase())
