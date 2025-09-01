089ca4e24cdfeedf586a310092543b77074b37d0
Ewan Donovan <ewan.donovan@digital.justice.gov.uk>
2025-09-01 15:33:57 +0100

    CDPS-1574 Cleaning up, potentially move to util class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/common/client/response/AddressPrisonDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/common/client/response/AddressPrisonDto.kt
@@ -1,9 +1,5 @@
 package uk.gov.justice.digital.hmpps.personintegrationapi.common.client.response
 
-import uk.gov.justice.digital.hmpps.personintegrationapi.common.dto.ReferenceDataValue
-import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.AddressTypeDto
-import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.response.AddressResponseDto
-import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.response.ContactResponseDto
 import java.time.LocalDate
 
 data class AddressPrisonDto(
@@ -28,52 +24,7 @@ data class AddressPrisonDto(
   val endDate: LocalDate?,
   val phones: List<Telephone>,
   val addressUsages: List<AddressUsage>,
-) {
-  fun toAddressResponseDto(personId: String) = AddressResponseDto(
-    addressId = this.addressId,
-    personId = personId,
-    uprn = null,
-    noFixedAbode = this.noFixedAddress,
-    subBuildingName = this.flat,
-    buildingNumber = null,
-    buildingName = this.premise,
-    thoroughfareName = this.street,
-    dependantLocality = this.locality,
-    postTown = this.townCode?.let {
-      ReferenceDataValue("CITY_$it", it, this.town!!)
-    },
-    county = this.countyCode?.let {
-      ReferenceDataValue("COUNTY_$it", it, this.county!!)
-    },
-    country = this.countryCode?.let {
-      ReferenceDataValue("COUNTRY_$it", it, this.country!!)
-    },
-    postCode = this.postalCode,
-    fromDate = this.startDate,
-    toDate = this.endDate,
-    addressTypes = this.addressUsages.map { usage ->
-      AddressTypeDto(
-        active = usage.activeFlag,
-        addressUsageType = ReferenceDataValue(
-          "ADDRESS_TYPE_${usage.addressUsage}",
-          usage.addressUsage,
-          usage.addressUsageDescription,
-        ),
-      )
-    },
-    primaryAddress = this.primary,
-    postalAddress = this.mail,
-    comment = this.comment,
-    addressPhoneNumbers = this.phones.map { phone ->
-      ContactResponseDto(
-        contactId = phone.phoneId,
-        contactType = phone.type,
-        contactValue = phone.number,
-        contactPhoneExtension = phone.ext,
-      )
-    },
-  )
-}
+)
 
 data class Telephone(
   val phoneId: Long,
--- src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/corepersonrecord/service/PersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/corepersonrecord/service/PersonService.kt
@@ -4,10 +4,16 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.PrisonApiClient
 import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.request.SourceSystem.NOMIS
+import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.response.AddressPrisonDto
 import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.response.CorePersonRecordAlias
 import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.response.CorePersonRecordReferenceDataValue
 import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.response.DistinguishingMarkPrisonDto
+import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.response.FullPersonPrisonDto
+import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.response.MilitaryRecordPrisonDto
+import uk.gov.justice.digital.hmpps.personintegrationapi.common.client.response.PhysicalAttributesPrisonDto
 import uk.gov.justice.digital.hmpps.personintegrationapi.common.dto.ReferenceDataValue
+import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.AddressTypeDto
+import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.response.AddressResponseDto
 import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.response.ContactResponseDto
 import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.response.DistinguishingMarkDto
 import uk.gov.justice.digital.hmpps.personintegrationapi.corepersonrecord.dto.response.DistinguishingMarkImageDetail
@@ -28,67 +34,62 @@ class PersonService(
       return ResponseEntity.status(response.statusCode).build()
     }
 
-    val contacts = buildList {
-      body.phones.forEach { phone ->
-        add(ContactResponseDto(phone.phoneId, phone.type, phone.number, phone.ext))
-      }
-      body.emails.forEach { email ->
-        add(ContactResponseDto(email.emailAddressId, "EMAIL", email.email))
-      }
-    }
-
-    // Military
-    val rankSuffixList = setOf("\\(Army\\)", "\\(Navy\\)", "\\(RAF\\)", "\\(Royal Marines\\)")
-    val rankSuffixPattern = Regex(rankSuffixList.joinToString("|"), RegexOption.IGNORE_CASE)
-    val militaryRecords = body.militaryRecord.militaryRecords.map {
-      MilitaryRecordDto(
-        militarySeq = it.militarySeq,
-        warZoneCode = it.warZoneCode,
-        warZoneDescription = it.warZoneDescription,
-        startDate = it.startDate,
-        endDate = it.endDate,
-        militaryDischargeCode = it.militaryDischargeCode,
-        militaryDischargeDescription = it.militaryDischargeDescription,
-        militaryBranchCode = it.militaryBranchCode,
-        militaryBranchDescription = it.militaryBranchDescription,
-        description = it.description,
-        unitNumber = it.unitNumber,
-        enlistmentLocation = it.enlistmentLocation,
-        dischargeLocation = it.dischargeLocation,
-        selectiveServicesFlag = it.selectiveServicesFlag,
-        militaryRankCode = it.militaryRankCode,
-        militaryRankDescription = it.militaryRankDescription?.replace(rankSuffixPattern, "")?.trim(),
-        serviceNumber = it.serviceNumber,
-        disciplinaryActionCode = it.disciplinaryActionCode,
-        disciplinaryActionDescription = it.disciplinaryActionDescription,
-      )
-    }
-
-    //Phys att
-    val physicalAttributes =
-      PhysicalAttributesDto(
-        height = body.physicalAttributes.height,
-        weight = body.physicalAttributes.weight,
-        hair = body.physicalAttributes.hair?.toReferenceDataValue(),
-        facialHair = body.physicalAttributes.facialHair?.toReferenceDataValue(),
-        face = body.physicalAttributes.face?.toReferenceDataValue(),
-        build = body.physicalAttributes.build?.toReferenceDataValue(),
-        leftEyeColour = body.physicalAttributes.leftEyeColour?.toReferenceDataValue(),
-        rightEyeColour = body.physicalAttributes.rightEyeColour?.toReferenceDataValue(),
-        shoeSize = body.physicalAttributes.shoeSize,
-      )
-
     val mappedResponse = FullPersonResponseDto(
-      addresses = body.addresses.map { it.toAddressResponseDto(prisonerNumber) },
+      addresses = body.addresses.map { it.toResponseDto(prisonerNumber) },
       pseudonyms = body.aliases.map { it.toResponseDto() },
-      contacts = contacts.toMutableList(),
-      militaryRecords = militaryRecords,
-      physicalAttributes = physicalAttributes,
-      distinguishingMarks = body.distinguishingMarks.map { it.toDto() },
+      contacts = body.toContactsResponseDto(),
+      militaryRecords = body.militaryRecord.toResponseDto(),
+      physicalAttributes = body.physicalAttributes.toResponseDto(),
+      distinguishingMarks = body.distinguishingMarks.map { it.toResponseDto() },
     )
 
     return ResponseEntity.ok(mappedResponse)
   }
+
+  private fun AddressPrisonDto.toResponseDto(personId: String) = AddressResponseDto(
+    addressId = this.addressId,
+    personId = personId,
+    uprn = null,
+    noFixedAbode = this.noFixedAddress,
+    subBuildingName = this.flat,
+    buildingNumber = null,
+    buildingName = this.premise,
+    thoroughfareName = this.street,
+    dependantLocality = this.locality,
+    postTown = this.townCode?.let {
+      ReferenceDataValue("CITY_$it", it, this.town!!)
+    },
+    county = this.countyCode?.let {
+      ReferenceDataValue("COUNTY_$it", it, this.county!!)
+    },
+    country = this.countryCode?.let {
+      ReferenceDataValue("COUNTRY_$it", it, this.country!!)
+    },
+    postCode = this.postalCode,
+    fromDate = this.startDate,
+    toDate = this.endDate,
+    addressTypes = this.addressUsages.map { usage ->
+      AddressTypeDto(
+        active = usage.activeFlag,
+        addressUsageType = ReferenceDataValue(
+          "ADDRESS_TYPE_${usage.addressUsage}",
+          usage.addressUsage,
+          usage.addressUsageDescription,
+        ),
+      )
+    },
+    primaryAddress = this.primary,
+    postalAddress = this.mail,
+    comment = this.comment,
+    addressPhoneNumbers = this.phones.map { phone ->
+      ContactResponseDto(
+        contactId = phone.phoneId,
+        contactType = phone.type,
+        contactValue = phone.number,
+        contactPhoneExtension = phone.ext,
+      )
+    },
+  )
 
   private fun CorePersonRecordAlias.toResponseDto(): PseudonymResponseDto = PseudonymResponseDto(
     sourceSystemId = offenderId,
@@ -106,13 +107,66 @@ class PersonService(
     ethnicity = ethnicity?.toReferenceDataValue(),
   )
 
+  private fun FullPersonPrisonDto.toContactsResponseDto(): List<ContactResponseDto> {
+    return buildList {
+      phones.forEach { phone ->
+        add(ContactResponseDto(phone.phoneId, phone.type, phone.number, phone.ext))
+      }
+      emails.forEach { email ->
+        add(ContactResponseDto(email.emailAddressId, "EMAIL", email.email))
+      }
+    }
+  }
+
+  private fun MilitaryRecordPrisonDto.toResponseDto(): List<MilitaryRecordDto> {
+    val rankSuffixList = setOf("\\(Army\\)", "\\(Navy\\)", "\\(RAF\\)", "\\(Royal Marines\\)")
+    val rankSuffixPattern = Regex(rankSuffixList.joinToString("|"), RegexOption.IGNORE_CASE)
+    return this.militaryRecords.map {
+      MilitaryRecordDto(
+        militarySeq = it.militarySeq,
+        warZoneCode = it.warZoneCode,
+        warZoneDescription = it.warZoneDescription,
+        startDate = it.startDate,
+        endDate = it.endDate,
+        militaryDischargeCode = it.militaryDischargeCode,
+        militaryDischargeDescription = it.militaryDischargeDescription,
+        militaryBranchCode = it.militaryBranchCode,
+        militaryBranchDescription = it.militaryBranchDescription,
+        description = it.description,
+        unitNumber = it.unitNumber,
+        enlistmentLocation = it.enlistmentLocation,
+        dischargeLocation = it.dischargeLocation,
+        selectiveServicesFlag = it.selectiveServicesFlag,
+        militaryRankCode = it.militaryRankCode,
+        militaryRankDescription = it.militaryRankDescription
+          ?.replace(rankSuffixPattern, "")
+          ?.trim(),
+        serviceNumber = it.serviceNumber,
+        disciplinaryActionCode = it.disciplinaryActionCode,
+        disciplinaryActionDescription = it.disciplinaryActionDescription,
+      )
+    }
+  }
+
+  private fun PhysicalAttributesPrisonDto.toResponseDto() = PhysicalAttributesDto(
+    height = height,
+    weight = weight,
+    hair = hair?.toReferenceDataValue(),
+    facialHair = facialHair?.toReferenceDataValue(),
+    face = face?.toReferenceDataValue(),
+    build = build?.toReferenceDataValue(),
+    leftEyeColour = leftEyeColour?.toReferenceDataValue(),
+    rightEyeColour = rightEyeColour?.toReferenceDataValue(),
+    shoeSize = shoeSize,
+  )
+
   private fun CorePersonRecordReferenceDataValue.toReferenceDataValue() = ReferenceDataValue(
     id = "${domain}_$code",
     code,
     description,
   )
 
-  private fun DistinguishingMarkPrisonDto.toDto(): DistinguishingMarkDto = DistinguishingMarkDto(
+  private fun DistinguishingMarkPrisonDto.toResponseDto(): DistinguishingMarkDto = DistinguishingMarkDto(
     id = id,
     bookingId = bookingId,
     offenderNo = offenderNo,
