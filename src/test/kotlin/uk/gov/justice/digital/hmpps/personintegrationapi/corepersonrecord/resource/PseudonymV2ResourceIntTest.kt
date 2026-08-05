d3100742edf756956d11325a2f62b17a0147a888
Nick Johnson <136325256+nj963@users.noreply.github.com>
2026-08-05 15:53:37 +0100

    CDPS-1978 Scale back validation on name fields (#166)
    * CDPS-1978 Scale back validation on name fields
    
    * CDPS-1978 Linting

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/corepersonrecord/resource/PseudonymV2ResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personintegrationapi/corepersonrecord/resource/PseudonymV2ResourceIntTest.kt
@@ -113,12 +113,6 @@ class PseudonymV2ResourceIntTest : IntegrationTestBase() {
       @Nested
       @DisplayName("First name")
       inner class FirstName {
-        @Test
-        internal fun `first name must only contain valid characters`() {
-          expectBadRequest(createRequest(firstName = "@@@"))
-            .jsonPath("$.userMessage")
-            .isEqualTo("Field: firstName - First name is not valid")
-        }
 
         @Test
         internal fun `first name can not be greater than 35 characters`() {
@@ -138,12 +132,6 @@ class PseudonymV2ResourceIntTest : IntegrationTestBase() {
       @Nested
       @DisplayName("Last name")
       inner class LastName {
-        @Test
-        internal fun `last name must only contain valid characters`() {
-          expectBadRequest(createRequest(lastName = "###"))
-            .jsonPath("$.userMessage")
-            .isEqualTo("Field: lastName - Last name is not valid")
-        }
 
         @Test
         internal fun `last name can not be greater than 35 characters`() {
@@ -165,13 +153,6 @@ class PseudonymV2ResourceIntTest : IntegrationTestBase() {
       inner class MiddleName1 {
 
         @Test
-        internal fun `first middle name must only contain valid characters`() {
-          expectBadRequest(createRequest(middleName1 = "@@@"))
-            .jsonPath("$.userMessage")
-            .isEqualTo("Field: middleName1 - Middle name 1 is not valid")
-        }
-
-        @Test
         internal fun `first middle can not be greater than 35 characters`() {
           expectBadRequest(createRequest(middleName1 = "A".repeat(36)))
             .jsonPath("$.userMessage")
@@ -182,13 +163,6 @@ class PseudonymV2ResourceIntTest : IntegrationTestBase() {
       @Nested
       @DisplayName("Middle name 2")
       inner class MiddleName2 {
-
-        @Test
-        internal fun `second middle name must only contain valid characters`() {
-          expectBadRequest(createRequest(middleName2 = "@@@"))
-            .jsonPath("$.userMessage")
-            .isEqualTo("Field: middleName2 - Middle name 2 is not valid")
-        }
 
         @Test
         internal fun `second middle can not be greater than 35 characters`() {
