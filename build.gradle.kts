a493e127c569a01a0092ebc40469e92549ff7b7b
Igor <igor.ushkarev1@justice.gov.uk>
2026-08-20 11:47:24 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0")
   constraints {
-    implementation("org.webjars:swagger-ui:5.32.2")
+    implementation("org.webjars:swagger-ui:5.32.11")
   }
   implementation("io.sentry:sentry-spring-boot-4:8.53.0")
 
